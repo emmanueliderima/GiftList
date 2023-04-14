8390adf7a4b6aff0a324bf50bed1a1bc5a486e63
EmmaJS <iderimaemmanuel24@gmail.com>
2023-04-14 10:11:58 -0700

    Started Project

--- client/index.js
+++ client/index.js
@@ -1,14 +1,20 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
+
+const Tree = new MerkleTree(niceList);
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
+  const name = "Paula Pagac DVM";
+  const proof = Tree.getProof(niceList.findIndex((n) => n === name));
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    proof,
+    name,
   });
 
   console.log({ gift });
