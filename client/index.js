c759943070f7613c7a3fda7ec023f6ab65df7cfc
EmmaJS <iderimaemmanuel24@gmail.com>
2023-04-21 17:58:26 -0700

    Made Application Interactive

--- client/index.js
+++ client/index.js
@@ -2,13 +2,23 @@ const axios = require("axios");
 const niceList = require("../utils/niceList.json");
 const MerkleTree = require("../utils/MerkleTree");
 
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const serverUrl = "http://localhost:1225";
 
 const Tree = new MerkleTree(niceList);
 
-async function main() {
+readline.question("Enter Your Name: ", (name) => {
+  main(name);
+  readline.close();
+});
+
+async function main(inputName) {
   // TODO: how do we prove to the server we're on the nice list?
-  const name = "Paula Pagac DVM";
+  const name = inputName;
   const proof = Tree.getProof(niceList.findIndex((n) => n === name));
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
@@ -19,5 +29,3 @@ async function main() {
 
   console.log({ gift });
 }
-
-main();
